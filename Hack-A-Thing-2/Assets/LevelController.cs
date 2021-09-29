d2de19e7e38f99ba2a5b46b017787feddad5e8d9
dgrkotsonis <63176396+dgrkotsonis@users.noreply.github.com>
2021-09-29 13:48:29 -0400

    finished project levels 1 and 2, may add more shortly

---
+++ Hack-A-Thing-2/Assets/LevelController.cs
@@ -0,0 +1,38 @@
+// using System.Collections;
+// using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+
+    private static int _nextLevelIndex = 1;
+    private Enemy[] _enemies;
+
+    // call when level is enabled
+    private void OnEnable()
+    {
+        _enemies = FindObjectsOfType<Enemy>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // check if enemies are all dead
+        foreach(Enemy enemy in _enemies)
+        {
+            if (enemy != null)
+            {
+                return;
+            }
+        }
+
+        Debug.Log("You killed all enemies");
+
+        // load the next level
+        _nextLevelIndex ++;
+        string nextLevelName = "Level" + _nextLevelIndex;
+        SceneManager.LoadScene(nextLevelName);
+    }
+}
